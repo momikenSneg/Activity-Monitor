bb646189783bc0c1844c9d4db4d54275cba4095b
Kate <momiken98@gmail.com>
2019-12-07 13:44:34 +0700

    EntityTypeConfiguration for Models/Developer.cs

--- Activity-Monitor/ActivityMonitor.Database/Models/Developer.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/Developer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,5 +14,29 @@ namespace ActivityMonitor.Database.Models
         public string Email { get; set; }
         public ICollection<DeveloperRepository> Repositories { get; set; }
         public ICollection<Commit> Commits { get; set; }
+    }
+
+    class DeveloperConfig : IEntityTypeConfiguration<Developer>
+    {
+        public void Configure(EntityTypeBuilder<Developer> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Email)
+                .IsRequired();
+            builder
+                .HasIndex(x => x.Email)
+                .IsUnique();
+
+            builder
+                .Property(x => x.FirstName)
+                .IsRequired();
+
+            builder
+                .Property(x => x.LastName)
+                .IsRequired();
+        }
     }
 }
