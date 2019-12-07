06c86e26c617a311dbf375ec1b3d762f05413b34
Kate <momiken98@gmail.com>
2019-12-07 13:21:37 +0700

    Models/Repository added

--- Activity-Monitor/ActivityMonitor/Program.cs
+++ Activity-Monitor/ActivityMonitor/Program.cs
@@ -6,7 +6,7 @@ namespace ActivityMonitor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Activity monitor!");
         }
     }
 }
