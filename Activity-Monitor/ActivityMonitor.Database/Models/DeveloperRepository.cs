3b2bbcc18d3807528f659624dab8d6113c65884f
Kate <momiken98@gmail.com>
2019-12-07 13:40:58 +0700

    EntityTypeConfiguration  for Models/DeveloperRepository.cs

--- Activity-Monitor/ActivityMonitor.Database/Models/DeveloperRepository.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/DeveloperRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,5 +12,26 @@ namespace ActivityMonitor.Database.Models
         public Developer Developer { get; set; }
         public Guid RepositoryId { get; set; }
         public Repository Repository { get; set; }
+    }
+
+    class DeveloperRepositoryConfiguration : IEntityTypeConfiguration<DeveloperRepository>
+    {
+        public void Configure(EntityTypeBuilder<DeveloperRepository> builder)
+        {
+            builder
+                .HasKey(x => new { x.DeveloperId, x.RepositoryId });
+
+            builder
+                .HasOne(x => x.Developer)
+                .WithMany(x => x.Repositories)
+                .HasForeignKey(x => x.DeveloperId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.Repository)
+                .WithMany(x => x.Developers)
+                .HasForeignKey(x => x.RepositoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
