dfa393e9fcd45e75e300bd49ce89021fd506656b
Kate <momiken98@gmail.com>
2019-12-07 13:58:05 +0700

    Models/CodeComplexity added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/CodeComplexity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class CodeComplexity
+    {
+        public Guid Id { get; set; }
+        public Guid RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+        public DateTimeOffset CalculatedAt { get; set; }
+        public int Value { get; set; }
+    }
+}
