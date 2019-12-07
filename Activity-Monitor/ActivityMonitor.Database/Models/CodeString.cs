31ad278df1c16533d0b213b5a806552bebadc039
Kate <momiken98@gmail.com>
2019-12-07 13:54:47 +0700

    EntityTypeConfiguration

--- Activity-Monitor/ActivityMonitor.Database/Models/CodeString.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/CodeString.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,5 +15,32 @@ namespace ActivityMonitor.Database.Models
         public Commit CreationCommit { get; set; }
         public Guid? DeleteCommitId { get; set; }
         public Commit DeleteCommit { get; set; }
+    }
+
+    class CodeStringConfig : IEntityTypeConfiguration<CodeString>
+    {
+        public void Configure(EntityTypeBuilder<CodeString> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .HasOne(x => x.File)
+                .WithMany(x => x.Strings)
+                .HasForeignKey(x => x.FileId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.CreationCommit)
+                .WithMany(x => x.CreatedStrings)
+                .HasForeignKey(x => x.CreationCommitId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.DeleteCommit)
+                .WithMany(x => x.DeletedStrings)
+                .HasForeignKey(x => x.DeleteCommitId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
