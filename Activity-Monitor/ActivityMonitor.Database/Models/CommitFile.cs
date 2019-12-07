f244307c6e170aa03db54f1ea09fa46eea43f2f2
Kate <momiken98@gmail.com>
2019-12-07 13:49:50 +0700

    Models/CommitFile added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/CommitFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class CommitFile
+    {
+        public Guid CommitId { get; set; }
+        public Commit Commit { get; set; }
+        public Guid FileId { get; set; }
+        public File File { get; set; }
+        public int Action { get; set; }
+
+    }
+}
