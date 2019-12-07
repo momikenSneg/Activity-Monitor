27cded7fb4a0e1fc607d8f9a0d4480c5e95f30e7
Kate <momiken98@gmail.com>
2019-12-07 13:46:38 +0700

    Models/Commit.cs added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/Commit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class Commit
+    {
+        public Guid Id { get; set; }
+        public string GitId { get; set; }
+        public Guid AuthorId { get; set; }
+        public Developer Author { get; set; }
+        public Guid RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public ICollection<CommitFile> Files { get; set; }
+        public ICollection<CodeString> CreatedStrings { get; set; }
+        public ICollection<CodeString> DeletedStrings { get; set; }
+    }
+}
