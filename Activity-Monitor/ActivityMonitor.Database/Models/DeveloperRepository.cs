65514e3cf085773b9c2f721b6db7f6d81a825694
Kate <momiken98@gmail.com>
2019-12-07 13:32:38 +0700

    Models/DeveloperRepository added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/DeveloperRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class DeveloperRepository
+    {
+        public Guid DeveloperId { get; set; }
+        public Developer Developer { get; set; }
+        public Guid RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+    }
+}
