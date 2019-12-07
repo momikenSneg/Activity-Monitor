a02241247a9d8269b6fe129f3cefdb8ec71267b1
Kate <momiken98@gmail.com>
2019-12-07 14:51:33 +0700

    interface APMT

---
+++ Activity-Monitor/ActivityMonitor.PMT/APMT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityMonitor.PMT
+{
+    /// <summary>
+    /// Common interface for project managment tools
+    /// </summary>
+    public interface APMT
+    {
+        string[] GetProjects();
+        Task<string[]> GetUsers(string project);
+    }
+}
