fc1e986bfd8370e2d4ccbd6fc8b70374a3158fba
Kate <momiken98@gmail.com>
2019-12-07 13:23:28 +0700

    EntityTypeConfiguration fo Repository

--- Activity-Monitor/ActivityMonitor.Database/Models/Repository.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/Repository.cs
@@ -6,5 +6,28 @@ namespace ActivityMonitor.Database.Models
 {
     class Repository
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<DeveloperRepository> Developers { get; set; }
+        public ICollection<Commit> Commits { get; set; }
+        public ICollection<CodeComplexity> CodeComplexityMeasures { get; set; }
+        public ICollection<File> Files { get; set; }
+    }
+
+    class RepositoryConfig : IEntityTypeConfiguration<Repository>
+    {
+        public void Configure(EntityTypeBuilder<Repository> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Name)
+                .IsRequired();
+
+            builder
+                .HasIndex(x => x.Name)
+                .IsUnique();
+        }
     }
 }
