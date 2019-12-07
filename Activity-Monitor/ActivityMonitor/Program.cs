2c19652d3433c2e0e5d4e8381d1e9caa6843ad8e
Kate <momiken98@gmail.com>
2019-12-07 12:13:23 +0700

    config database and start

---
+++ Activity-Monitor/ActivityMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActivityMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
