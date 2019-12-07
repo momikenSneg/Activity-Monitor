57f46a66b1db9a0eb3d766ba28adce83d4541efb
Kate <momiken98@gmail.com>
2019-12-07 13:50:57 +0700

    FileAction more comfortable

--- Activity-Monitor/ActivityMonitor.Database/Models/CommitFile.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/CommitFile.cs
@@ -4,13 +4,19 @@ using System.Text;
 
 namespace ActivityMonitor.Database.Models
 {
+    public enum FileAction
+    {
+        Created = 0,
+        Modified = 1,
+        Deleted = 2
+    }
     public class CommitFile
     {
         public Guid CommitId { get; set; }
         public Commit Commit { get; set; }
         public Guid FileId { get; set; }
         public File File { get; set; }
-        public int Action { get; set; }
+        public FileAction Action { get; set; }
 
     }
 }
