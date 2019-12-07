80690cc4150d6461c0f722e1960a79cb24b668f4
Kate <momiken98@gmail.com>
2019-12-07 13:43:16 +0700

    Models/Developer.cs added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class Developer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<DeveloperRepository> Repositories { get; set; }
+        public ICollection<Commit> Commits { get; set; }
+    }
+}
