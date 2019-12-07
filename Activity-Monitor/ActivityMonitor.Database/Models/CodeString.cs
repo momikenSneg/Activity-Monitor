d793e7b41b0fd506a3573b2369033986ede3e1c3
Kate <momiken98@gmail.com>
2019-12-07 13:53:25 +0700

    Models/CodeString.cs added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/CodeString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    public class CodeString
+    {
+        public Guid Id { get; set; }
+        public Guid FileId { get; set; }
+        public File File { get; set; }
+        public Guid CreationCommitId { get; set; }
+        public Commit CreationCommit { get; set; }
+        public Guid? DeleteCommitId { get; set; }
+        public Commit DeleteCommit { get; set; }
+    }
+}
