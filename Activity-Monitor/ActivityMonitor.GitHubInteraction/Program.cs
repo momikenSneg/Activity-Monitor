8bf78d4533a24ed7eee887b63b08f98b878b38b5
Kate <momiken98@gmail.com>
2019-12-20 03:07:03 +0700

    github interaction

---
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActivityMonitor.GitHubInteraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
