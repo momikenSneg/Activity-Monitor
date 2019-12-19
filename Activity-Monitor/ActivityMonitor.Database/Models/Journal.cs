2d47a0061668d8ae069cc6038af25280dd53df84
Kate <momiken98@gmail.com>
2019-12-19 13:22:51 +0700

    Journal config added

--- Activity-Monitor/ActivityMonitor.Database/Models/Journal.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/Journal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,5 +16,39 @@ namespace ActivityMonitor.Database.Models
         public string OldValue { get; set; }
         public string NewValue { get; set; }
         public int IssueId { get; set; }
+    }
+    class JournalConfiguration : IEntityTypeConfiguration<Journal>
+    {
+        public void Configure(EntityTypeBuilder<Journal> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Notes)
+                .IsRequired();
+
+            builder
+                .Property(x => x.CreatedOn)
+                .IsRequired();
+
+            builder
+                .Property(x => x.NameChange)
+                .IsRequired();
+
+            builder
+                .Property(x => x.OldValue)
+                .IsRequired();
+
+            builder
+                .Property(x => x.NewValue)
+                .IsRequired();
+
+            builder
+                .HasIndex(x => x.IssueId);
+
+            builder
+                .HasIndex(x => x.AuthorId);
+        }
     }
 }
