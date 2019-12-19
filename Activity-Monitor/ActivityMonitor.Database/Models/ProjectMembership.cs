f891c553b2aa4db14729b6390a341e3f51d62511
Kate <momiken98@gmail.com>
2019-12-19 13:13:44 +0700

    ProjectMembership Configure added

--- Activity-Monitor/ActivityMonitor.Database/Models/ProjectMembership.cs
+++ Activity-Monitor/ActivityMonitor.Database/Models/ProjectMembership.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,5 +12,26 @@ namespace ActivityMonitor.Database.Models
         public Project Project { get; set; }
         public int MembershipId { get; set; }
         public Membership Membership { get; set; }
+    }
+
+    class ProjectMembershipConfig : IEntityTypeConfiguration<ProjectMembership>
+    {
+        public void Configure(EntityTypeBuilder<ProjectMembership> builder)
+        {
+            builder
+                .HasKey(x => new { x.ProjectId, x.MembershipId });
+
+            builder
+                .HasOne(x => x.Project)
+                .WithMany(x => x.Memberships)
+                .HasForeignKey(x => x.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.Membership)
+                .WithMany(x => x.Projects)
+                .HasForeignKey(x => x.MembershipId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
