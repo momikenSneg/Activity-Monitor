f5507c789cf55abdfde1558e93b74d8c896f2492
Kate <momiken98@gmail.com>
2019-12-19 13:20:37 +0700

    Journal model added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/Journal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    class Journal
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string NameChange { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public int IssueId { get; set; }
+    }
+}
