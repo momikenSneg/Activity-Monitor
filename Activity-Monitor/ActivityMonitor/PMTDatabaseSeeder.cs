fd18f38262bdab6de38eb8ba5f635d6bb8bd772c
Kate <momiken98@gmail.com>
2019-12-20 03:56:49 +0700

    PTMDatabaseSeeder constructor added

--- Activity-Monitor/ActivityMonitor/PMTDatabaseSeeder.cs
+++ Activity-Monitor/ActivityMonitor/PMTDatabaseSeeder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ActivityMonitor.Database;
+using ActivityMonitor.PMT;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,15 @@ namespace ActivityMonitor
 {
     class PMTDatabaseSeeder : IDatabaseSeeder
     {
+        private ActivityContext context;
+        private List<string> prj;
+        private APMT pmt;
+        public PMTDatabaseSeeder(ActivityContext c, List<string> projects, string name, string password, string url)
+        {
+            context = c;
+            prj = projects;
+            pmt = new PMT.PMT(name, password, new Uri(url));
+        }
         public void Seed()
         {
             throw new NotImplementedException();
