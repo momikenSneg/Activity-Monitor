e926b982616c038bd9957c218ee33231c0726e49
Kate <momiken98@gmail.com>
2019-12-19 13:47:32 +0700

    PMT extend APMT

---
+++ Activity-Monitor/ActivityMonitor.PMT/PMT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityMonitor.PMT
+{
+    //class PMT : APMT
+    //{
+    //    public override string[] GetProjects()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public override Task<string[]> GetUsers(string project)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
