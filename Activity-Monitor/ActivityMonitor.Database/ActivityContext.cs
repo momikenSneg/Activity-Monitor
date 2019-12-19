b41cf48794508377c787651670e4615f7d5e58fe
Kate <momiken98@gmail.com>
2019-12-20 03:27:01 +0700

    DbSets for redmine

--- Activity-Monitor/ActivityMonitor.Database/ActivityContext.cs
+++ Activity-Monitor/ActivityMonitor.Database/ActivityContext.cs
@@ -19,6 +19,12 @@ namespace ActivityMonitor.Database
         public DbSet<Developer> Developers { get; set; }
         public DbSet<File> Files { get; set; }
         public DbSet<Repository> Repositories { get; set; }
+        // Redmine
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Journal> Journals { get; set; }
+        public DbSet<ProjectMembership> ProjectMemberships { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
