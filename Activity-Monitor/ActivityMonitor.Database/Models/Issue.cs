087fa97b95a3dbf7caf4687d06039460ac225f04
Kate <momiken98@gmail.com>
2019-12-19 13:18:09 +0700

    model Issue.cs added

---
+++ Activity-Monitor/ActivityMonitor.Database/Models/Issue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMonitor.Database.Models
+{
+    class Issue
+    {
+        public int Id { get; set; }
+        public string TrackerName { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public int ProjectId { get; set; }
+    }
+}
