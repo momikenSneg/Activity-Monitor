df4256747d7c2eaf0f1c8ba57d43d7493ece7ac1
Kate <momiken98@gmail.com>
2019-12-20 03:11:52 +0700

    AuthGitInfo change

--- Activity-Monitor/ActivityMonitor.GitHubInteraction/AuthGitInfo.cs
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/AuthGitInfo.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
 namespace ActivityMonitor.GitHubInteraction
 {
+    [DataContract]
     class AuthGitInfo
     {
+        [DataMember]
+        public string login { get; set; }
+        [DataMember]
+        public string password { get; set; }
+        [DataMember]
+        public RepositoryAttribute[] repositories { get; set; }
     }
 }
