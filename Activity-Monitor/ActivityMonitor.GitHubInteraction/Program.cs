4bc3b7af12a35b51452a4f2797ac70fdf5c0bfdc
Kate <momiken98@gmail.com>
2019-12-21 02:55:22 +0700

    json init

--- Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
@@ -4,6 +4,32 @@ namespace ActivityMonitor.GitHubInteraction
 {
     class Program
     {
+        public static void InitJson()
+        {
+            var am = new RepositoryAttribute
+            {
+                owner = "Phoenix-616",
+                name = "Activity-Monitor"
+            };
+            var cg = new RepositoryAttribute
+            {
+                owner = "ogresed",
+                name = "CardGame"
+            };
+            RepositoryAttribute[] repositorys = new RepositoryAttribute[2];
+            repositorys[0] = am;
+            repositorys[1] = cg;
+            var a = new AuthGitInfo
+            {
+                login = "ogresed",
+                password = "enterPasswod",
+                repositories = repositorys
+            };
+
+            var stream1 = File.OpenWrite("gitAuth.json");
+            var ser = new DataContractJsonSerializer(typeof(AuthGitInfo));
+            ser.WriteObject(stream1, a);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
