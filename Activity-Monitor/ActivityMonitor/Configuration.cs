d1d55bcb30e9c805f04e43edaf87b0bfbae6a77a
Kate <momiken98@gmail.com>
2019-12-21 03:30:38 +0700

    made config structure

--- Activity-Monitor/ActivityMonitor/Configuration.cs
+++ Activity-Monitor/ActivityMonitor/Configuration.cs
@@ -6,5 +6,11 @@ namespace ActivityMonitor
 {
     class Configuration
     {
+        public string PMTUri { get; set; }
+        public string PMTLogin { get; set; }
+        public string PMTPassword { get; set; }
+        public List<string> PMTProjects { get; set; }
+
     }
 }
+
