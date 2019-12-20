5830cf57e61b68b4e1e060d8be01586283e5ace4
Kate <momiken98@gmail.com>
2019-12-21 02:58:24 +0700

    main should be async Task

--- Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
+++ Activity-Monitor/ActivityMonitor.GitHubInteraction/Program.cs
@@ -32,7 +32,7 @@ namespace ActivityMonitor.GitHubInteraction
             var ser = new DataContractJsonSerializer(typeof(AuthGitInfo));
             ser.WriteObject(stream1, a);
         }
-        static void Main(string[] args)
+        static static async Task Main(string[] args)
         {
             AuthGitInfo info;
             using (Stream fs = GetStream())
